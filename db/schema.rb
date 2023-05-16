18174b3592e328a56859803ba292bb65c7c2486c
Ali Eyvazli <ali.eyvazli@kellogg.northwestern.edu>
2023-05-16 03:53:24 +0000

    C2

--- db/schema.rb
+++ db/schema.rb
@@ -1,30 +1,12 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_225503) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.date "posted_on"
-    t.integer "place_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_033920) do
+  create_table "locations", force: :cascade do |t|
+    t.string "City_name"
+    t.string "Country_name"
+    t.date "When"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
